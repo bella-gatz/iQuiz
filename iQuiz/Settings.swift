b4fe4eab6503eb6f086e91fdb2de34f6e3652e83
bella-gatz <102693135+bella-gatz@users.noreply.github.com>
2025-05-02 14:40:34 -0700

    added tableView

---
+++ iQuiz/Settings.swift
@@ -0,0 +1,29 @@
+//
+//  Settings.swift
+//  iQuiz
+//
+//  Created by Bella Gatzemeier on 5/2/25.
+//
+
+import UIKit
+
+class Settings: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class tableCell: UITableViewCell {
+    @IBOutlet weak var cellName: UILabel!
+    
+}
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+        @IBOutlet weak var tableView: UITableView!
+    
+    var cellName = ["Mathematics", "Marvel Super Heroes", "Science"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+    // sets up table size rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellName.count
+    }
 
+    // sets up data in each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! tableCell
+        cell.cellName.text = cellName[indexPath.row]
+        return cell
+    }
+    
+    
+    // height of row at every index
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    
+    @IBAction func settingstapped(_ sender: Any) {
+        // create the alert
+        let alert = UIAlertController(title: "My Title", message: "This is my message.", preferredStyle: UIAlertController.Style.alert)
+
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
