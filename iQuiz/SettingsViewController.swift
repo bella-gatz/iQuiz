fbf7dba4118acb8c93e013cac5d17c0abb5851d7
bella-gatz <102693135+bella-gatz@users.noreply.github.com>
2025-05-19 18:59:20 -0700

    added app settings

--- iQuiz/SettingsViewController.swift
+++ iQuiz/SettingsViewController.swift
@@ -13,25 +13,34 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        goodURL = "https://tednewardsandbox.site44.com/questions.json"
-        url.text = goodURL
+        let defaults = UserDefaults.standard
+        let stored = defaults.string(forKey: "quizDataURL")
+          ?? "https://tednewardsandbox.site44.com/questions.json"
+        goodURL = stored
+        urlTextField.text = stored
+        quizzes = loadFromFile()
         // Do any additional setup after loading the view.
     }
     
-    var reqQuizzes: [Quiz] = []
-    var reqQs: [QuizQuestion] = []
+    var quizzes: [Quiz] = []
     var goodURL: String?
     
-    @IBOutlet weak var url: UITextField!
+    @IBOutlet weak var urlTextField: UITextField!
     
-    @IBAction func newHttp(_ sender: Any) {
-        var url = url.text
-        guard !url!.isEmpty else {
+    @IBAction func newHttp(_ sender : Any) {
+        var urlString = urlTextField.text
+        guard !urlString!.isEmpty else {
             callError()
             return
         }
         
-        var request = URLRequest(url: URL(string: url!)!)
+        guard let requestURL = URL(string: urlString!) else {
+            print("invalid url string")
+            callError()
+            return
+        }
+        
+        var request = URLRequest(url: requestURL)
         request.httpMethod = "GET"
         
         (URLSession.shared.dataTask(with: request) { data, response, error
@@ -53,30 +62,63 @@ class SettingsViewController: UIViewController {
                 } else {
                     do {
                         let quizzes = try JSONDecoder().decode([Quiz].self, from: data!)
-                        
+                        self.saveToFile(quiz: quizzes)
+                        self.goodURL = urlString
+                        UserDefaults.standard.set(urlString, forKey: "quizDataURL")
                     } catch {
                         print("Error in JSON decoding: \(error.localizedDescription)")
                         self.callError()
-                        return
                     }
-                    self.goodURL = url
+                    return
                 }
-                
               }
             }
         }).resume()
     }
 
     func callError() {
-        let alert = UIAlertController(title: "Network Error", message: "Invalid URL", preferredStyle: UIAlertController.Style.alert)
+        let alert = UIAlertController(title: "Data Fetch Failed", message: "Invalid URL", preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    func saveToFile(quiz : [Quiz]) {
+        let file = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("jsonData.json")
+        
+        do {
+            let data = try JSONEncoder().encode(quiz)
+            try data.write(to: file)
+            print("Saved JSON data to url \(file)")
+        } catch {
+            print("Error saving JSON data: \(error)")
+        }
+    }
+    
+    func loadFromFile() -> [Quiz] {
+        let file = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("jsonData.json")
+//        let fileContents = try FileManager.default.contentsOfDirectory(atPath: "jsonData.json")
+        
+        do {
+            let data = try Data(contentsOf: file)
+            let contents = try JSONDecoder().decode([Quiz].self, from: data)
+            print("Loaded data from \(file)")
+            return contents
+        } catch {
+            print("Error loading JSON data: \(error)")
+            return []
+        }
+    }
+    
+    
+    @IBAction func openSettings(_ sender: Any) {
+        UIApplication.shared.open(URL.init(string: UIApplication.openSettingsURLString)!)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "tomain" {
             if let destinationVC = segue.destination as? ViewController {
                 destinationVC.defaultURL = goodURL!
+                destinationVC.quizzes = quizzes
             }
         }
     }
